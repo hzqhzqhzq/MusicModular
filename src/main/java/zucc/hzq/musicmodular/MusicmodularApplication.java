403f4fd9c75b355f27774530413171036616c2b3
hzqhzqhzq <1493152259@qq.com>
2019-02-17 22:32:59 +0800

    first push

---
+++ src/main/java/zucc/hzq/musicmodular/MusicmodularApplication.java
@@ -0,0 +1,13 @@
+package zucc.hzq.musicmodular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicmodularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicmodularApplication.class, args);
+    }
+
+}
