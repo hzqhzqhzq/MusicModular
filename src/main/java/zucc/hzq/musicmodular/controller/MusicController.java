382d7f6dd03c6a9bcb24be87b0f7ab38c0a656bb
zhenqinghe <zhenqinghe@capirobo.com>
2019-02-21 17:29:52 +0800

    初始化

---
+++ src/main/java/zucc/hzq/musicmodular/controller/MusicController.java
@@ -0,0 +1,11 @@
+package zucc.hzq.musicmodular.controller;
+
+/**
+ * @Auther: zhenqinghe
+ * @Date: Created in 10:17 AM 2/19/2019
+ * @Description:
+ * @Modified By:
+ */
+public class MusicController {
+
+}
